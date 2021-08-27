9572a43ff7d093937b9b35837883a105db33b0d0
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-08-27 12:50:53 +0200

    Added login page

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,44 @@
+(function () {
+
+    function checkLogin() {
+        const form = document.getElementById("form");
+
+        if (form.checkValidity()) {
+            makeCall("POST", 'CheckLogin', form,
+
+                function (response) {
+                    if (response.readyState === XMLHttpRequest.DONE) {
+                        const message = response.responseText;
+
+                        switch (response.status) {
+                            case HttpResponseStatus.OK:
+                                sessionStorage.setItem('username', message);
+                                window.location.href = "home.html";
+                                document.getElementById("errorMsg").style.visibility = "hidden";
+                                break;
+
+                            case HttpResponseStatus.BAD_REQUEST:
+                            case HttpResponseStatus.UNAUTHORIZED:
+                            case HttpResponseStatus.INTERNAL_SERVER_ERROR:
+                            case HttpResponseStatus.BAD_GATEWAY:
+                            default:
+                                document.getElementById("errorMsg").textContent = message;
+                                document.getElementById("errorMsg").style.visibility = "visible";
+                                break;
+                        }
+                    }
+                }
+            );
+        }
+        else {
+            form.reportValidity();
+        }
+    }
+
+    document.getElementById("button").addEventListener("click", () => checkLogin());
+    document.addEventListener("keypress", (e) => {
+        if(e.key === "Enter" && e.altKey === false &&  e.ctrlKey === false)
+            checkLogin();
+    });
+
+})();
