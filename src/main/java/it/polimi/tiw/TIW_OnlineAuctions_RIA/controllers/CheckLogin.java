a29f0d988a1349dd60a775fde26312ad0c043b11
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-08-27 12:51:40 +0200

    Added CheckLogin servlet

--- src/main/java/it/polimi/tiw/TIW_OnlineAuctions_RIA/controllers/CheckLogin.java
+++ src/main/java/it/polimi/tiw/TIW_OnlineAuctions_RIA/controllers/CheckLogin.java
@@ -1,19 +1,67 @@
 package it.polimi.tiw.TIW_OnlineAuctions_RIA.controllers;
 
+import it.polimi.tiw.TIW_OnlineAuctions_RIA.beans.User;
+import it.polimi.tiw.TIW_OnlineAuctions_RIA.dao.UserDAO;
+import it.polimi.tiw.TIW_OnlineAuctions_RIA.utils.Pair;
+import it.polimi.tiw.TIW_OnlineAuctions_RIA.utils.ServletUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "CheckLogin", value = "/CheckLogin")
+@MultipartConfig
 public class CheckLogin extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private Connection connection;
 
+    public void init() throws ServletException {
+        Pair<TemplateEngine, Connection> setupResult = ServletUtils.setupServlet(getServletContext());
+        this.connection = setupResult.getSecond();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email;
+        String password;
 
+        try {
+            email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+            password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+
+            if (email == null || password == null || email.isEmpty() || password.isEmpty())
+                throw new Exception("Email and password are required");
+
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().println(e.getMessage());
+            return;
+        }
+
+        UserDAO userDAO = new UserDAO(connection);
+        User user;
+        try {
+            user = userDAO.authenticateUser(email, password);
+        } catch (SQLException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+            response.getWriter().println("Failure in database credential checking");
+            return;
+        }
+
+        if(user == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println("Incorrect credentials");
+        }
+        else {
+            request.getSession().setAttribute("user", user);
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().println(user);
+        }
     }
 }
