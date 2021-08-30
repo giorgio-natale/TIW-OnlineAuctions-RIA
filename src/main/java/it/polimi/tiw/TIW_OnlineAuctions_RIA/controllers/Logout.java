2b93b21027f7158de2fff654b60a468750d939d0
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-08-30 21:27:44 +0200

    Added logout servlet

---
+++ src/main/java/it/polimi/tiw/TIW_OnlineAuctions_RIA/controllers/Logout.java
@@ -0,0 +1,28 @@
+package it.polimi.tiw.TIW_OnlineAuctions_RIA.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "Logout", value = "/Logout")
+public class Logout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+
+        if (session != null)
+            session.invalidate();
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println("Logout successful");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+}
