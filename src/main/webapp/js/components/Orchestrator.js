c011fb65c3ddcf593359f68814f7d316c094cef3
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-08-30 17:08:37 +0200

    Added new components to Orchestrator

--- src/main/webapp/js/components/Orchestrator.js
+++ src/main/webapp/js/components/Orchestrator.js
@@ -1,64 +1,104 @@
+import {SearchBar} from "./SearchBar.js";
 import {FoundAuctionsList} from "./FoundAuctionsList.js";
-import {AuctionDetails} from "./AuctionDetails.js";
-import {SearchBar} from "./SearchBar.js"
 import {WonAuctionsList} from "./WonAuctionsList.js";
+
+// NewAuctionForm
 import {OpenAuctionsList} from "./OpenAuctionsList.js";
 import {ClosedAuctionsList} from "./ClosedAuctionsList.js";
+
+import {AuctionDetails} from "./AuctionDetails.js";
 import {WinnerDetails} from "./WinnerDetails.js";
+// NewBidForm
+// BidsList
 
 
 //It creates all the components. The init function shows (for now) only the search form. This "class" contains a function
 //for each possible interaction (almost equivalent to all the 'GoTo...' servlets of the pure html project).
 //Each function is responsible to show and hide components in order to compose the correct layout.
 //This component is passed to all other subcomponents so that they can invoke the correct interaction function when needed
-export function Orchestrator(){
-    this.foundAuctionsList = new FoundAuctionsList(document.getElementById("section-found"), this);
-    this.auctionDetails = new AuctionDetails(document.getElementById("section-auctionDetails"), this);
+export function Orchestrator() {
+    // this.navBar = new NavBar(document.getElementById("section-navbar"), this);
+
     this.searchBar = new SearchBar(document.getElementById("section-search"), this);
+    this.foundAuctionsList = new FoundAuctionsList(document.getElementById("section-found"), this);
     this.wonAuctionsList = new WonAuctionsList(document.getElementById("section-won"), this);
+
+    // this.newAuctionForm = new NewAuctionForm(document.getElementById("section-newAuction"), this);
     this.openAuctionsList = new OpenAuctionsList(document.getElementById("section-open"), this);
     this.closedAuctionsList = new ClosedAuctionsList(document.getElementById("section-closed"), this);
+
+    this.auctionDetails = new AuctionDetails(document.getElementById("section-auctionDetails"), this);
     this.winnerDetails = new WinnerDetails(document.getElementById("section-auctionWinner"), this);
+    // this.newBidForm = new NewBidForm(document.getElementById("section-addBid"), this);
+    // this.bidsList = new BidsList(document.getElementById("section-bids"), this);
 
     let self = this;
 
-    this.init = function(){
-        self.auctionDetails.reset();
-        self.foundAuctionsList.reset();
+    this.init = function() {
+        // self.navBar.show();
+
         self.searchBar.show();
+        self.foundAuctionsList.reset();
         self.wonAuctionsList.show();
+
+        // self.newAuctionForm.reset();
+        self.openAuctionsList.reset();
+        self.closedAuctionsList.reset();
+
+        self.auctionDetails.reset();
+        self.winnerDetails.reset();
+        // self.newBidForm.reset();
+        // self.bidsList.reset();
+    }
+
+    this.showSearchResults = function(searchString) {
+        // self.navBar.show();
+
+        self.searchBar.show();
+        self.foundAuctionsList.show(searchString);
+        self.wonAuctionsList.show();
+
+        // self.newAuctionForm.reset();
+        self.openAuctionsList.reset();
+        self.closedAuctionsList.reset();
+
+        self.auctionDetails.reset();
+        self.winnerDetails.reset();
+        // self.newBidForm.reset();
+        // self.bidsList.reset();
+    }
+
+    this.showSellPage = function () {
+        // self.navBar.show();
+
+        self.searchBar.reset();
+        self.foundAuctionsList.reset();
+        self.wonAuctionsList.reset();
+
+        // self.newAuctionForm.show();
         self.openAuctionsList.show();
         self.closedAuctionsList.show();
-        self.winnerDetails.show();
-    }
 
-    this.showSearchResults = function(searchString){
         self.auctionDetails.reset();
-        self.foundAuctionsList.show(searchString);
-        self.searchBar.show();
-        self.wonAuctionsList.show();
-        self.openAuctionsList.reset();
-        self.closedAuctionsList.reset();
         self.winnerDetails.reset();
+        // self.newBidForm.reset();
+        // self.bidsList.reset();
     }
 
-    this.showDetails = function(_details){
-        self.auctionDetails.show(_details);
-        self.foundAuctionsList.reset();
+    this.showDetailsAndBids = function(auctionID) {
+        // self.navBar.show();
+
         self.searchBar.reset();
+        self.foundAuctionsList.reset();
         self.wonAuctionsList.reset();
+
+        // self.newAuctionForm.reset();
         self.openAuctionsList.reset();
         self.closedAuctionsList.reset();
-        self.winnerDetails.reset();
-    }
 
-    this.showOffers = function(_id){
-        //TODO: implement
-        alert("Showing the offers for auction #" + _id);
-    }
-
-    this.makeOffer = function(_id){
-        //TODO: implement
-        alert("Showing the layout to make an offer for auction #" + _id);
+        self.auctionDetails.show(auctionID);
+        self.winnerDetails.show();
+        // self.newBidForm.show();
+        // self.bidsList.show();
     }
 }
