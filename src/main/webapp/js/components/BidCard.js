d01dd6c7a6a00e59756e69f35140e15227687d8a
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-08-31 00:56:18 +0200

    Added BidCard component

---
+++ src/main/webapp/js/components/BidCard.js
@@ -0,0 +1,71 @@
+export function BidCard(){
+    let self = this;
+
+    self.container = document.createElement("li");
+    self.container.className += "list-group-item";
+
+    self.createTemplate = function(){
+        let outerDiv = document.createElement("div");
+
+        let h5 = document.createElement("h5");
+        h5.className += "card-title d-inline-block";
+
+        //name of the bid owner
+        self.userSpan = document.createElement("span");
+        self.userSpan.className += "badge text-light fw-normal";
+
+        let innerDiv = document.createElement("div");
+
+        let outerBidPriceSpan = document.createElement("span");
+        outerBidPriceSpan.className += "fw-bold";
+
+        //offer amount
+        self.priceBidSpan = document.createElement("span");
+
+        let euroSpan = document.createElement("span");
+        euroSpan.innerHTML = " &euro;"
+
+        //bid date
+        self.dateSpan = document.createElement("span");
+
+
+        //assembling tags
+        self.container.appendChild(outerDiv);
+        outerDiv.appendChild(h5);
+        h5.appendChild(self.userSpan);
+        outerDiv.appendChild(innerDiv);
+        innerDiv.appendChild(outerBidPriceSpan);
+        outerBidPriceSpan.appendChild(self.priceBidSpan);
+        outerBidPriceSpan.appendChild(euroSpan);
+        innerDiv.appendChild(self.dateSpan);
+    }
+
+    self.show = function(bid){
+        self.reset();
+        self.createTemplate();
+        self.update(bid);
+    }
+
+    self.update = function(bid) {
+        if(localStorage.getItem("user_id") !== String(bid.user_id)){
+            self.userSpan.className += " bg-primary";
+            self.userSpan.textContent = bid.user_first_name + " " + bid.user_last_name;
+        }else{
+            self.userSpan.className += " bg-secondary";
+            self.userSpan.textContent = bid.user_first_name + " " + bid.user_last_name + " (You)";
+        }
+        self.priceBidSpan.textContent = bid.price;
+        self.dateSpan.textContent = ' - ' + secondsToDate(bid.bid_time);
+    }
+
+    self.reset = function() {
+        while (self.container.firstChild) {
+            self.container.firstChild.remove();
+        }
+    }
+
+    self.cleanup = function(){
+        self.container.parentNode.removeChild(self.container);
+    }
+
+}
