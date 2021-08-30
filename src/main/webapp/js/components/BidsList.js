a93d0ac8f0d8a8f60f14fafd91b7627bea2863a7
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-08-31 00:56:48 +0200

    Added BidsList component

---
+++ src/main/webapp/js/components/BidsList.js
@@ -0,0 +1,57 @@
+import {BidCard} from "./BidCard.js";
+
+export function BidsList(_container, _orchestrator){
+    let self = this;
+
+    self.container = _container;
+    self.orchestrator = _orchestrator;
+
+    self.listContainer = document.getElementById("list-bids");
+    self.emptyContainer = document.getElementById("bids-empty");
+    self.bidCards = [];
+
+    self.show = function(_auctionId){
+        makeCall("GET", "GetBids?auctionId=" + _auctionId, null,
+            function (request) {
+                if (request.status === HttpResponseStatus.OK) {
+                    self.update(JSON.parse(request.responseText));
+                }
+                else {
+                    // TODO: error handling
+                    alert("Error " + request.status + ": " + request.responseText);
+                }
+            }
+        );
+    }
+
+    self.update = function(_bids){
+        self.reset();
+        self.container.style.display = "";
+
+        if(_bids.length > 0){
+            self.emptyContainer.style.display = "none";
+            self.listContainer.style.display = "";
+        }else{
+            self.emptyContainer.style.display = "";
+            self.listContainer.style.display = "none";
+        }
+
+        _bids.forEach((bid) => {
+            let bidCard = new BidCard();
+            self.bidCards.push(bidCard);
+
+            self.listContainer.appendChild(bidCard.container);
+            bidCard.show(bid);
+        });
+
+
+    }
+
+    self.reset = function(){
+        self.container.style.display = "none";
+        self.bidCards.forEach((c) => {
+            c.cleanup();
+        });
+        self.bidCards = [];
+    }
+}
