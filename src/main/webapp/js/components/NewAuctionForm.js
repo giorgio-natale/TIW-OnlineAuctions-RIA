a86cd1c9dab3ca21e5b36768da5958eabb04592a
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-09-01 12:48:15 +0200

    Added form validation

--- src/main/webapp/js/components/NewAuctionForm.js
+++ src/main/webapp/js/components/NewAuctionForm.js
@@ -7,31 +7,40 @@ export function NewAuctionForm(_container, _orchestrator) {
     self.form = document.getElementById("newAuction-form");
 
     function submitForm() {
-        if (self.form.checkValidity()) {
-            makeCall("POST", 'AddAuction', new FormData(self.form),
-                function (request) {
-                    if (request.status === HttpResponseStatus.OK) {
-                        self.orchestrator.showSellPage();
-                        self.form.reset();
-                    }
-                    else {
-                        // TODO: error handling
-                        alert("Error " + request.status + ": " + request.responseText);
-                    }
-                },
-                true
-            );
-        }
-        else {
-            self.form.reportValidity();
-        }
+        makeCall("POST", 'AddAuction', new FormData(self.form),
+            function (request) {
+                if (request.status === HttpResponseStatus.OK) {
+                    self.orchestrator.showSellPage();
+                    self.form.reset();
+                }
+                else {
+                    // TODO: error handling
+                    alert("Error " + request.status + ": " + request.responseText);
+                }
+            },
+            true
+        );
     }
 
     this.registerEvents = function() {
-        self.form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            submitForm();
-        });
+        self.form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            self.form.classList.add("was-validated");
+
+            if (!self.form.checkValidity()) {
+                event.stopPropagation();
+            }
+            else {
+                submitForm();
+                self.form.classList.remove("was-validated");
+            }
+        },
+        false);
+
+        let dateInput = document.getElementById("newAuction-auctionEndDate");
+        dateInput.min = new Date().addDays(1).toISOString().slice(0, 10);
+        dateInput.max = new Date().addDays(366).toISOString().slice(0, 10);
     }
 
     this.reset = function(){
--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -1,3 +1,9 @@
+Date.prototype.addDays = function(days) {
+    const date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
 function makeCall(method, url, formData, callback, kickIfUnauthorized = true) {
     const request = new XMLHttpRequest();
 
